35c2dc9a2db4e55a4a4fc0e0d8f1f1ece065091d
lfbl <fabel932@gmail.com>
2026-03-11 13:33:27 +0100

    deleted some files

--- SEA-ME-Students-main/Head_Unit-Yocto/Head_Unit_jun/modules/call/service/CallService.cpp
+++
@@ -1,5 +0,0 @@
-#include "CallService.h"
-
-CallService::CallService(QObject *parent) : QObject(parent) {}
-void CallService::startCall() { m_inCall = true;  emit callStateChanged(true); }
-void CallService::endCall()   { m_inCall = false; emit callStateChanged(false); }
--- SEA-ME-Students-main/warmup/module02/ex01/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "Car.hpp"
-
-
-int main() {
-    Car myCar;
-    myCar.PrintParts();
-    return 0;
-}
